a6e31c1bd75971a6be328f1aba0643c19c19c5ac
James Sutton <1892175+zeryx@users.noreply.github.com>
2020-05-05 16:15:19 -0500

    uncomment code

--- src/SentimentPipeline.py
+++ src/SentimentPipeline.py
@@ -14,9 +14,9 @@ def apply(input):
         else:
             raise Exception(
                 "input {} is not a valid data API input, or you don't have permission to access it".format(input))
-    # elif isinstance(input, list):
-    #     # if the input a list of data URIs, lets skip directory scanning and process each URI normally
-    #     files = input
+    elif isinstance(input, list):
+        # if the input a list of data URIs, lets skip directory scanning and process each URI normally
+        files = input
     else:
         raise Exception("input must be of type str or list, found {}".format(str(type(input))))
 
