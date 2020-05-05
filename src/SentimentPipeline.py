15c2613184862abfaec497fd4646178d7a250f08
James Sutton <1892175+zeryx@users.noreply.github.com>
2020-05-05 11:22:58 -0500

    added basic path checks, added github CI integration with simple test

--- src/SentimentPipeline.py
+++ src/SentimentPipeline.py
@@ -6,33 +6,36 @@ import Algorithmia
 def apply(path):
     output = {}
     client = Algorithmia.client()
-    for file in client.dir(path).files():
-        # extract text from image using OCR
-        body = client.algo("util/ExtractText/0.1.1").pipe("data://" + file.path).result
-        body = body.rstrip("\n")
+    if client.dir(path).exists():
+        for file in client.dir(path).files():
+            # extract text from image using OCR
+            body = client.algo("util/ExtractText/0.1.1").pipe("data://" + file.path).result
+            body = body.rstrip("\n")
 
-        # determine language of text
-        lang = client.algo("nlp/LanguageIdentification/1.0.0").pipe({"sentence": body}).result
+            # determine language of text
+            lang = client.algo("nlp/LanguageIdentification/1.0.0").pipe({"sentence": body}).result
 
-        # translate non-English text
-        if lang != 'en':
-            input = {"action": "translate", "text": body}
-            body = client.algo("translation/GoogleTranslate/0.1.1").pipe(input).result["translation"]
+            # translate non-English text
+            if lang != 'en':
+                input = {"action": "translate", "text": body}
+                body = client.algo("translation/GoogleTranslate/0.1.1").pipe(input).result["translation"]
 
-        # Tokenize text for analysis by sentence
-        token_body = client.algo("ApacheOpenNLP/SentenceDetection/0.1.0").pipe(body).result
+            # Tokenize text for analysis by sentence
+            token_body = client.algo("ApacheOpenNLP/SentenceDetection/0.1.0").pipe(body).result
 
-        # determine sentiment of tokenized text
-        keyword_sentiment = []
-        for token in token_body:
-            print("data://" + file.path + " -> " + token)
-            sent = client.algo("nlp/SentimentAnalysis/1.0.5").pipe({"document": token}).result[0]["sentiment"]
-            keyword_sentiment.append(sent)
+            # determine sentiment of tokenized text
+            keyword_sentiment = []
+            for token in token_body:
+                print("data://" + file.path + " -> " + token)
+                sent = client.algo("nlp/SentimentAnalysis/1.0.5").pipe({"document": token}).result[0]["sentiment"]
+                keyword_sentiment.append(sent)
 
-        avg_sent = sum(keyword_sentiment) / len(keyword_sentiment)
+            avg_sent = sum(keyword_sentiment) / len(keyword_sentiment)
 
-        # record output
-        output["data://" + file.path] = {"average sentiment": avg_sent}
+            # record output
+            output["data://" + file.path] = {"average sentiment": avg_sent}
+    else:
+        raise Exception("input {} is not a valid data API path, or you don't have permission to access it".format(path))
 
     return output
 
